7a46e418d53ec822e3530f863c8bedf42126ead6
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-10-22 12:33:25 +0100

    Agregar archivos de proyecto.

---
+++ UTF2-EJER6/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Windows;
+
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace UTF2_EJER6
+{
+
+    public partial class MainWindow : Window, IComponentConnector
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+
+
+       
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (imagen1Image.IsMouseOver)
+            {
+                imagen1Image.Opacity = 1;
+            }
+            else
+                imagen1Image.Opacity = 0.5;
+            if (imagen2Image.IsMouseOver)
+            {
+                imagen2Image.Opacity = 1;
+            }
+            else
+                imagen2Image.Opacity = 0.5;
+            if (imagen3Image.IsMouseOver)
+            {
+                imagen3Image.Opacity = 1;
+            }
+            else
+                imagen3Image.Opacity = 0.5;
+        }
+    }
+}
